76d0cf0c1e88faca5d990ef9995b53ea0a40fe51
yunzed <yunzed@gmail.com>
2013-03-25 01:12:04 +0800

    add script for depends.

--- depends/build-ffmpeg.bat
+++ depends/build-ffmpeg.bat
@@ -1,1 +1,13 @@
-./configure --enable-shared --enable-small --disable-static --bindir=../../bin/ --libdir=../../bin/ --incdir=../../bin/include --disable-ffplay --enable-libx264 --enable-gpl --extra-cflags=-le:/github/vcap.git/bin/include --extra-ldflags=e:/github/vcap.git/bin/
+./configure --enable-memalign-hack --enable-shared --enable-small --disable-static --incdir=../../bin/include --disable-ffplay --disable-everything --enable-encoder=libx264 --enable-encoder=aac --enable-muxer=mpegts --enable-libx264 --enable-gpl --extra-cflags=-Ie:/github/vcap.git/bin/include/x264 --extra-ldflags=-Le:/github/vcap.git/bin/
+
+make install
+
+#copy libs
+cp libavformat/avformat.lib ../../bin
+cp libavcodec/avcodec.lib ../../bin
+cp libavutil/avutil.lib ../../bin
+
+#copy dlls
+cp libavformat/avformat-54.dll ../../bin
+cp libavcodec/avcodec-54.dll ../../bin
+cp libavutil/avutil-52.dll ../../bin
