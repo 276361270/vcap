069cc0cdbb4763e37dd4bf2cb271750f0653fd92
yunzed <yunzed@gmail.com>
2013-03-24 23:27:32 +0800

    add depends.

--- depends/x264/build.bat
+++ depends/x264/build.bat
@@ -1,1 +1,6 @@
-./configure --disable-cli --enable-shared --enable-win32thread --bindir=../../bin --libdir=../../
+./configure --disable-cli --enable-shared --enable-win32thread --bindir=../../bin --libdir=../../bin --extra-ldflags=-Wl,--output-def=libx264-130.def 
+make
+LIB /DEF:libx264-130.def 
+make install
+cp x264.h ../../bin/include/x264/
+cp x264_config.h ../../bin/include/x264/
